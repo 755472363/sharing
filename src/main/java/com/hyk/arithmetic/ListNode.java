818e2651a554fab750a7e42d62be7750fc194323
haoyukui <haoyukui@58.com>
2022-04-02 12:13:50 +0800

    修改 ListNode 为public

--- src/main/java/com/hyk/arithmetic/ListNode.java
+++ src/main/java/com/hyk/arithmetic/ListNode.java
@@ -1,11 +1,11 @@
 package com.hyk.arithmetic;
 
 
-class ListNode {
-    int val;
-    ListNode next = null;
+public class ListNode {
+    public int val;
+    public ListNode next = null;
 
-    ListNode(int val) {
+    public ListNode(int val) {
         this.val = val;
     }
 }
