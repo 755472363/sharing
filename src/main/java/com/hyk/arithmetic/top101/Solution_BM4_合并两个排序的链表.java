a6982ff05a90731a2cc4b2edc4ee8f5d8b0277ef
haoyukui <haoyukui@58.com>
2022-06-14 15:43:49 +0800

    BM4 合并两个排序的链表

--- src/main/java/com/hyk/arithmetic/top101/Solution_BM4_合并两个排序的链表.java
+++ src/main/java/com/hyk/arithmetic/top101/Solution_BM4_合并两个排序的链表.java
@@ -16,62 +16,75 @@ public class Solution_BM4_合并两个排序的链表 {
      * 输入{-1,2,4},{1,3,4}时，合并后的链表为{-1,1,2,3,4,4}，所以对应的输出为{-1,1,2,3,4,4}
      */
 
+    // 如果使用虚拟头节点，会简单一些
     public ListNode Merge2(ListNode list1, ListNode list2) {
-        if (list1 != null && list2 == null) {
+        if (list2 == null) {
             return list1;
         }
-        if (list1 == null && list2 != null) {
+        if (list1 == null) {
             return list2;
         }
-        ListNode headNode;
-        ListNode curNode;
-        if (list1.val == list2.val) {
-            headNode = new ListNode(list1.val);
-            curNode = headNode;
-            curNode.next = new ListNode(list2.val);
-            curNode = curNode.next;
-        } else {
-            if (list1.val > list2.val) {
-                headNode = new ListNode(list2.val);
-                list2 = list2.next;
+        ListNode newHead = null;
+        ListNode newCurrHead = null;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                if (newHead == null) {
+                    newHead = new ListNode(list1.val);
+                    newCurrHead = newHead;
+                } else {
+                    newCurrHead.next = new ListNode(list1.val);
+                    newCurrHead = newCurrHead.next;
+                }
+                list1 = list1.next;
             } else {
-                headNode = new ListNode(list1.val);
-                list1 = list1.next;
-            }
-            curNode = headNode;
-        }
-        while (list1.next != null && list2.next != null) {
-            if (list1.next.val == list2.next.val) {
-                curNode.next = new ListNode(list1.next.val);
-                curNode = curNode.next;
-                curNode.next = new ListNode(list2.next.val);
-                curNode = curNode.next;
-                list1 = list1.next;
-                list2 = list2.next;
-                continue;
-            }
-            curNode.next = new ListNode(list1.next.val > list2.next.val ? list2.next.val : list1.next.val);
-            curNode = curNode.next;
-            list1 = list1.next;
-            list2 = list2.next;
-        }
-        if (list1.next == null) {
-            while (list2.next != null) {
-                curNode.next = new ListNode(list2.next.val);
-                curNode = curNode.next;
+                if (newHead == null) {
+                    newHead = new ListNode(list2.val);
+                    newCurrHead = newHead;
+                } else {
+                    newCurrHead.next = new ListNode(list2.val);
+                    newCurrHead = newCurrHead.next;
+                }
                 list2 = list2.next;
             }
         }
-        if (list2.next == null) {
-            while (list1.next != null) {
-                curNode.next = new ListNode(list1.next.val);
-                curNode = curNode.next;
-                list1 = list1.next;
-            }
+        if (list1 == null) {
+            newCurrHead.next = list2;
+        } else if (list2 == null) {
+            newCurrHead.next = list1;
         }
-        return headNode;
+        return newHead;
     }
 
+    // 使用虚拟头节点
+    public ListNode Merge3(ListNode list1, ListNode list2) {
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        ListNode dummyHead = new ListNode(-1);
+        ListNode newCurrHead = dummyHead;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                newCurrHead.next = new ListNode(list1.val);
+                newCurrHead = newCurrHead.next;
+                list1 = list1.next;
+            } else {
+                newCurrHead.next = new ListNode(list2.val);
+                newCurrHead = newCurrHead.next;
+                list2 = list2.next;
+            }
+        }
+        if (list1 == null) {
+            newCurrHead.next = list2;
+        } else if (list2 == null) {
+            newCurrHead.next = list1;
+        }
+        return dummyHead.next;
+    }
+
+    // 递归
     public ListNode Merge(ListNode list1, ListNode list2) {
         if (list1 == null) {
             return list2;
@@ -102,8 +115,10 @@ public class Solution_BM4_合并两个排序的链表 {
                 curNode1 = curNode1.next;
             }
         }
+        check(headNode1);
+        check(headNode2);
 //        check(new Solution_BM4().Merge(headNode1, headNode1));
-        check(new Solution_BM4_合并两个排序的链表().Merge(headNode1, headNode2));
+        check(new Solution_BM4_合并两个排序的链表().Merge2(headNode1, headNode2));
     }
 
     private static void check(ListNode head) {
